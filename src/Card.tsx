282a23c8c5aea42ab15a02b90323892c6686bced
Aayush Shah <aayushshah1142@gmail.com>
2023-03-04 19:03:17 +0530

    fixed build errors

--- src/Card.tsx
+++ src/Card.tsx
@@ -5,7 +5,7 @@ function Card({coinName,currPrice,currChange,tvl,pairs,coinImg} : CardProps){
         <div className="flex flex-col align-center w-full mt-8">
             <div className="h-16 w-32 top-16 bg-[#14172b] mx-auto rounded-tl-xl rounded-bl-full rounded-br-full relative">
                 <div className="relative mx-auto rounded-full -top-12 h-24 w-24 bg-gradient-to-b from-[rgba(98,106,136,0.1)] to-[rgba(220,31,255,0.1)]">
-                    <img className="relative w-12 mx-auto top-6" src={`${coinImg}.png`}/>
+                    <img alt="coin logo" className="relative w-12 mx-auto top-6" src={`${coinImg}.png`}/>
                 </div>
             </div>
             <div className='bg-gradient-to-b from-[rgba(98,106,136,0.1)] to-[rgba(46,49,73,0.1)] rounded-3xl p-8 flex flex-col'>
@@ -28,7 +28,7 @@ function Card({coinName,currPrice,currChange,tvl,pairs,coinImg} : CardProps){
                 <p className="text-[#737BAE] font-semibold text-center mt-2 mb-4">TVL</p>
                 <div className="bg-[#14172b] w-full md:w-3/5 flex mx-auto justify-center rounded-[17px]">
                     {pairs.map((pair,id)=>
-                        <img key={id} className="mx-auto w-8 py-2 " src={`${pair}.png`}/>
+                        <img alt="popular pairing" key={id} className="mx-auto w-8 py-2 " src={`${pair}.png`}/>
                     )}
                 </div>
                 <p className="text-[#737BAE] font-semibold text-center mt-2">Popular Pairs</p>
