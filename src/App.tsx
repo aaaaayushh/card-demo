6a824566747c95d0ee5022efe8a29bb4fa7c7533
Aayush Shah <aayushshah1142@gmail.com>
2023-03-04 18:58:18 +0530

    finished design for assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
+
+export interface CardProps{
+  coinName: string;
+  currPrice: number;
+  currChange : number;
+  tvl: number;
+  pairs: Array<string>;
+  coinImg:string;
+}
 
 function App() {
+  const data : Array<CardProps> = [
+    {
+      coinName:"Solana (SOL)",
+      currChange:-12.35,
+      currPrice:2345,
+      tvl:60000,
+      pairs:["bitcoin","ethereum","binance"]
+      ,coinImg:"solana"
+    },
+    {
+      coinName:"Bitcoin (BTC)",
+      currChange:10,
+      currPrice:31812,
+      tvl:60000,
+      pairs:["solana","ethereum","binance"],
+    coinImg:"bitcoin"
+    },
+    {
+      coinName:"Ethereum (ETH)",
+      currChange:-11.93,
+      currPrice:1466.45,
+      tvl:60000,
+      pairs:["solana","bitcoin","binance"],
+      coinImg:"ethereum"
+    },
+    {
+      coinName:"Binance USD (BUSD)",
+      currChange: 0.26,
+      currPrice:1,
+      tvl:60000,
+      pairs:["solana","ethereum","binance"],
+      coinImg:"binance"
+    },
+    {
+      coinName:"Shiba Inu (SHIB)",
+      currChange: -8.1,
+      currPrice:0.00001948,
+      tvl:60000,
+      pairs:["solana","ethereum","binance"],
+      coinImg:"shiba"
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-wrap justify-center bg-[#14172b] h-full">
+
+      {data.map((card)=>(
+        <div className="w-full sm:w-5/12 md:w-4/12 lg:w-3/12 2xl:w-2/12 mx-8">
+          <Card coinName={card.coinName} currChange={card.currChange} currPrice={card.currPrice} tvl={card.tvl} pairs={card.pairs} coinImg={card.coinImg} />
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/Card.tsx
@@ -0,0 +1,40 @@
+import { CardProps } from "./App";
+
+function Card({coinName,currPrice,currChange,tvl,pairs,coinImg} : CardProps){
+    return(
+        <div className="flex flex-col align-center w-full mt-8">
+            <div className="h-16 w-32 top-16 bg-[#14172b] mx-auto rounded-tl-xl rounded-bl-full rounded-br-full relative">
+                <div className="relative mx-auto rounded-full -top-12 h-24 w-24 bg-gradient-to-b from-[rgba(98,106,136,0.1)] to-[rgba(220,31,255,0.1)]">
+                    <img className="relative w-12 mx-auto top-6" src={`${coinImg}.png`}/>
+                </div>
+            </div>
+            <div className='bg-gradient-to-b from-[rgba(98,106,136,0.1)] to-[rgba(46,49,73,0.1)] rounded-3xl p-8 flex flex-col'>
+                <p className="mt-12 mb-4 text-[#737BAE] font-semibold text-center">{coinName}</p>
+                <div className="flex bg-[#14172b] items-center text-white justify-center py-2 rounded-[17px] 2xl:shadow-inner">
+                    <p className={`invisible my-auto font-semibold text-sm`}>
+                        {currChange}%
+                    </p>
+                    <p className="mr-auto text-center flex-1">${currPrice > 0 ? currPrice.toLocaleString() : currPrice}</p>
+
+                    <p className={`ml-auto mr-2 ${currChange < 0 ? "text-red-500":"text-green-500"} my-auto font-semibold text-sm`}>
+                        {currChange}%
+                    </p>
+
+                </div>
+                <p className="mb-4 mt-2 text-[#737BAE] text-center font-semibold">Price</p>
+                <div className="flex py-2 bg-[#14172b] text-white justify-center rounded-[17px] 2xl:shadow-inner">
+                    <p className="text-center">${tvl > 0 ? tvl.toLocaleString() : tvl}</p>
+                </div>
+                <p className="text-[#737BAE] font-semibold text-center mt-2 mb-4">TVL</p>
+                <div className="bg-[#14172b] w-full md:w-3/5 flex mx-auto justify-center rounded-[17px]">
+                    {pairs.map((pair)=>
+                        <img className="mx-auto w-8 py-2 " src={`${pair}.png`}/>
+                    )}
+                </div>
+                <p className="text-[#737BAE] font-semibold text-center mt-2">Popular Pairs</p>
+                
+            </div>
+        </div>
+    );
+}
+export default Card;
